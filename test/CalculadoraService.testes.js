79193672eba4fc2994fcc1864c618a192483ad96
Leonam-Leao <leonamalbuquerqueleao@gmail.com>
2026-07-07 20:01:26 -0500

    testes mock

--- test/CalculadoraService.testes.js
+++ test/CalculadoraService.testes.js
@@ -1,15 +1,27 @@
 const {expect} = require("chai");
-const calculadoraService = require("../src/services/CalculadoraService");
+const {dividir} = require("../src/services/CalculadoraService");
+const sinon = require("sinon");
+const calculadora = require("../src/models/Calculadora");
 
 describe("Testes da CalculadoraService", () => {
+    afterEach(() => {
+        sinon.restore();
+    });
+
     it("Deve dividir dois números", () => {
-        const resultado = calculadoraService.dividir(10, 2);
+        const stub = sinon.stub(calculadora, "dividir").returns(5);
+
+        const resultado = dividir(10, 2);
+
+        sinon.assert.calledOnce(stub);
+        sinon.assert.calledWith(stub, 10, 2);
         expect(resultado).to.equal(5);
     });
 
     it("Deve lançar um erro ao dividir por zero", () => {
-        expect(() => calculadoraService.dividir(10, 0)).to.throw("Não é possível dividir por zero");
+        const stub = sinon.stub(calculadora, "dividir");
+
+        expect(() => dividir(10, 0)).to.throw("Não é possível dividir por zero");
+        sinon.assert.notCalled(stub);
     });
 })
-
-modeue
